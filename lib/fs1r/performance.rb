d10d7d677167911d762c36e87f6575c82ed33dd4
Dan Richert <dan.richert@gmail.com>
2011-09-07 14:28:08 -0400

    Add the rest of the Fs1r::Performance::Common methods

--- lib/fs1r/performance.rb
+++ lib/fs1r/performance.rb
@@ -100,33 +100,92 @@ module Fs1r
 
       # 0..127
       # 0-4: MIDI Clock
-      #   0: 1/4
-      #   1: 1/2
-      #   2: 1/1
-      #   3: 2/1
-      #   4: 4/1
       # 5-127: (10.0 - 500.0)
       def fseq_speed_ratio(v)
         tx 0x18, *byte_pair(v)
       end
 
-      def fseq_start_step_offset_hi; end
-      def fseq_start_step_offset_lo; end
+      # 0..4
+      # 0: 1/4
+      # 1: 1/2
+      # 2: 1/1
+      # 3: 2/1
+      # 4: 4/1
+      def midi_clock(v)
+        tx 0x19, *byte_pair(v)
+      end
 
-      def fseq_start_step_of_loop_point_hi; end
-      def fseq_start_step_of_loop_point_lo; end
+      # 0..127
+      def fseq_start_step_offset_hi(v)
+        tx 0x1A, *byte_pair(v)
+      end
 
-      def fseq_end_step_of_loop_point_hi; end
-      def fseq_end_step_of_loop_point_lo; end
+      # 0..127
+      def fseq_start_step_offset_lo(v)
+        tx 0x1B, *byte_pair(v)
+      end
 
-      def fseq_loop_mode; end
-      def fseq_play_mode; end
-      def fseq_velocity_sensitivity_for_tempo; end
-      def fseq_formant_pitch_mode; end
-      def fseq_key_on_trigger; end
-      def fseq_formant_sequence_delay; end
-      def fseq_level_level_velocity_sensitivity; end
+      # 0..127
+      def fseq_start_step_of_loop_point_hi(v)
+        tx 0x1C, *byte_pair(v)
+      end
 
+      # 0..127
+      def fseq_start_step_of_loop_point_lo(v)
+        tx 0x1D, *byte_pair(v)
+      end
+
+      # 0..127
+      def fseq_end_step_of_loop_point_hi(v)
+        tx 0x1E, *byte_pair(v)
+      end
+
+      # 0..127
+      def fseq_end_step_of_loop_point_lo(v)
+        tx 0x1F, *byte_pair(v)
+      end
+
+      # 0..1
+      # 0: one way
+      # 1: round
+      def fseq_loop_mode(v)
+        tx 0x20, *byte_pair(v)
+      end
+
+      # 1..2
+      # 1: scratch
+      # 2: fseq
+      def fseq_play_mode(v)
+        tx 0x21, *byte_pair(v)
+      end
+
+      # 0..7
+      def fseq_velocity_sensitivity_for_tempo(v)
+        tx 0x22, *byte_pair(v)
+      end
+
+      # 0..1
+      def fseq_formant_pitch_mode(v)
+        tx 0x23, *byte_pair(v)
+      end
+
+      # 0..1
+      # 0: first
+      # 1: all
+      def fseq_key_on_trigger(v)
+        tx 0x24, *byte_pair(v)
+      end
+
+      # 0..99
+      def fseq_formant_sequence_delay(v)
+        tx 0x26, *byte_pair(v)
+      end
+
+      # 0..127
+      # -64 - +63
+      def fseq_level_level_velocity_sensitivity
+        tx 0x27, *byte_pair(v)
+      end
 
       private
         def tx(pal, dvm, dvl)

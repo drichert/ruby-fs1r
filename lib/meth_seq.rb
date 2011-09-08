8f4a29e4cc95b2f4c90c74f0b073dc6d528f699e
Dan Richert <dan.richert@gmail.com>
2011-09-07 21:54:24 -0400

    Class::meth_seq works

--- lib/meth_seq.rb
+++ lib/meth_seq.rb
@@ -9,24 +9,22 @@ class Class
       :val_range       => 0..127  # valid parameter value range
     }
 
-    # raise Exception if spec has a missing param
-    spec.each {|name, params|
-      param_spec.merge params
-      params.each {|k, v|
-        raise Exception, "#{name}: missing :#{k}" if v.nil?
-      }
-    }
-
     spec.each do |meth, params|
+      # raise Exception if spec has a missing param
+      params = param_spec.merge params
+      params.each {|k, v|
+        raise Exception, "#{meth}: missing :#{k}" if v.nil?
+      }
+
       steps     = [params[:param_num_start]]
       step_size = params[:param_num_step]
-      (num_methods - 1).times {
-        steps << steps.last.to_i + step_size
+      (params[:num_methods] - 1).times {
+        steps << steps.last + step_size
       }
 
       name_num = params[:name_num_start]
       steps.each do |param_num|
-        define_method(m.gsub(/_n(_|$)/, "_#{name_num}\\1").to_sym) do |v|
+        define_method(meth.gsub(/_n(_|$)/, "_#{name_num}\\1").to_sym) do |v|
           tx param_num, *byte_pair(v)
         end
         name_num += 1
@@ -34,3 +32,13 @@ class Class
     end
   end
 end
+
+#class X
+#  meth_seq({
+#    'name_n' => {
+#      :num_methods     => 100,
+#      :param_num_start => 200
+#    }
+#  })
+#end
+#puts X.new.methods.grep /name/

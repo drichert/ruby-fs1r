440a4c0d4dd765be1a9ed72e3b629388172e1aec
Dan Richert <dan.richert@gmail.com>
2011-09-09 14:05:36 -0400

    wip: meth_seq specs

--- spec/lib/meth_seq_spec.rb
+++ spec/lib/meth_seq_spec.rb
@@ -1,60 +1,25 @@
 require 'spec_helper'
 
 describe Class do
-  before do
-    @meth_spec = {
-      :good => {
-        # All params
-        'good_n' => {
-          :num_methods     => 3,
-          :name_num_start  => 7,
-          :param_num_start => 0x12,
-          :param_num_step  => 2,
-          :val_range       => 0..48
-        },
-        # Required params only
-        'good_req_n' => {
-          :param_num_start => 0x0D
-        }
-      },
-      :good_single => {
-        'good_single_n' => {
-          :param_num_start => 0x01
-        }
-      },
-      :bad => {
-        # Missing required param
-        'bad_n' => {}
-      }
-    }
-  end
-
   describe "#meth_seq" do
     context "when sequence spec is valid" do
       before do
-        @meth_spec = {
-          # All params
-          'good_n' => {
-            :num_methods     => 3,
-            :name_num_start  => 7,
-            :param_num_start => 0x12,
-            :param_num_step  => 2,
-            :val_range       => 0..48
-          },
-          # Required params only
-          'good_req_n' => {
-            :param_num_start => 0x0D
-          },
-          :good_single => {
-            'good_single_n' => {
-              :param_num_start => 0x01
-            }
-          },
-        }
-
         @klass = Object.const_set 'Klass', Class.new
         @klass.class_eval do
-          meth_seq @meth_specs[:good]
+          meth_seq({
+            # All params
+            'good_n' => {
+              :num_methods     => 3,
+              :name_num_start  => 7,
+              :param_num_start => 0x12,
+              :param_num_step  => 2,
+              :val_range       => 0..48
+            },
+            # Required params only
+            'good_req_n' => {
+              :param_num_start => 0x0D
+            }
+          })
         end
 
         @obj_sans_extras = Object.new
@@ -71,7 +36,7 @@ describe Class do
 
     context "when sequence spec is invalid" do
       before do
-        @meth_spec =
+        @meth_spec = {
           :bad => {
             # Missing required param
             'bad_n' => {}

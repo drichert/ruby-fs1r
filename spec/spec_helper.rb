db64e87bff69e9be7aa063b3361aabd8420b908c
Dan Richert <dan.richert@gmail.com>
2011-11-16 20:55:42 -0500

    Add #sysex spec helper method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,22 @@ RSpec.configure do |cfg|
   #cfg.color_enabled = true
   #cfg.formatter     = "documentation"
 end
+
+def sysex(params = {})
+  [:pah, :pam, :pal, :dvm, :dvl].each {|param|
+    raise Exception, "missing #{param} param" if params[param].nil?
+  }
+
+  [
+    0xF0,
+    0x43,
+    Fs1r::device_id, # device number
+    0x5E,            # model id
+    params[:pah],    # parameter address high
+    params[:pam],    # parameter address middle
+    params[:pal],    # parameter address low
+    params[:dvm],    # data value most significant 7bit
+    params[:dvl],    # data value least significant 7bit
+    0xF7
+  ]
+end
